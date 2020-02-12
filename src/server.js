4fe66645730993cafd36bc010ccc28e9662358d9
Bartosz Mąka <bartoszmaka95@gmail.com>
2020-02-12 14:55:45 +0100

    Set configuration for compression middleware

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
-const expressStaticGzip = require("express-static-gzip");
+const compression = require('compression');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
-app.use('/', expressStaticGzip(path.join(__dirname, '..', 'build')))
+app.use(compression({level: 9, threshold: 1}))
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', '..', 'index.html'));

a3e561b067c2719fd7c9dad264d84e71d991efef
Bartosz Mąka <bartoszmaka95@gmail.com>
2020-02-14 13:54:32 +0100

    User sentry for error handling

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -6,10 +6,13 @@ import './App.css';
 import Router from './components/Router';
 import { store } from './redux/stores';
 
-// Sentry.init({
-//   dsn: "https://c0a1124b36e94a158b723fdbcefe2af0@sentry.io/2493924",
-//   release: `app@${process.env.commit || 'default'}`,
-// });
+const env = process.env.NODE_ENV || 'development'
+if (env !== 'development') {
+  Sentry.init({
+    dsn: "https://c0a1124b36e94a158b723fdbcefe2af0@sentry.io/2493924",
+    release: `app@${process.env.commit || 'default'}`,
+  });
+}
 
 function App() {
   return (

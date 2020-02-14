95a1efc73f8821e22b20b66d2270898d3cdd992a
Bartosz Mąka <bartoszmaka95@gmail.com>
2020-02-14 13:10:01 +0100

    Add graphql page

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import * as Sentry from '@sentry/browser';
 
 import './App.css';
 import Router from './components/Router';
 import { store } from './redux/stores';
+
+// Sentry.init({
+//   dsn: "https://c0a1124b36e94a158b723fdbcefe2af0@sentry.io/2493924",
+//   release: `app@${process.env.commit || 'default'}`,
+// });
 
 function App() {
   return (
--- react-ui/src/components/Home.js
+++ react-ui/src/components/Home.js
@@ -1,3 +1,40 @@
-const Home = () => 'Home page'
+import React, { useEffect, useState } from 'react';
+import { gql } from 'apollo-boost';
+
+import { client } from '../utils/graphql';
+
+const faultyFunction = _event => {
+  return (undefined).foo
+}
+
+const Home = () => {
+  const [queryResult, setQueryResult] = useState(null)
+
+  useEffect(() => {
+    client.query({
+      query: gql`
+        {
+          articles {
+            id,
+            title,
+            user {
+              email,
+              articlesCount
+            }
+          }
+        }
+      `
+    }).then(response => setQueryResult(response.data))
+      .catch(error => setQueryResult(error))
+  }, [])
+
+  return (
+    <div>
+      <div>Home page</div>
+      <button onClick={faultyFunction}>Raise an error</button>
+      <pre>{JSON.stringify(queryResult, null, 2)}</pre>
+    </div>
+  )
+}
 
 export default Home

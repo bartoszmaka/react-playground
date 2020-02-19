2fe95163f817b6aa2b57e2ad03d2b1907ceadbda
Bartosz Mąka <bartoszmaka95@gmail.com>
2020-02-19 14:03:40 +0100

    Add lazy imports

--- react-ui/src/components/Router.js
+++ react-ui/src/components/Router.js
@@ -1,22 +1,23 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux';
 import { Link, Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { get, isEqual } from 'lodash';
+import React, { useState, Suspense } from 'react'
 
-import ThemeContext, { themes } from '../contexts/themeContext';
+import { getCurrentUser } from '../redux/selectors/appState';
 import Boundary from '../wrappers/Boundary';
 import Home from './Home';
-import FormikForm from './FormikForm';
-import ReduxForm from './ReduxForm';
-import Settings from './Settings';
-import List from './List';
-import Admin from './Admin';
-import ErrorBoundary from './ErrorBoundary';
-import RequestCancellation from './RequestCancellation';
-import ModalsPage from './ModalsPage';
-import Login from './Login';
 import LoginCallback from './LoginCallback';
-import { getCurrentUser } from '../redux/selectors/appState';
+import ThemeContext, { themes } from '../contexts/themeContext';
+
+const FormikForm = React.lazy(() => import('./FormikForm'))
+const ReduxForm = React.lazy(() => import('./ReduxForm'))
+const Settings = React.lazy(() => import('./Settings'))
+const List = React.lazy(() => import('./List'))
+const Admin = React.lazy(() => import('./Admin'))
+const ErrorBoundary = React.lazy(() => import('./ErrorBoundary'))
+const RequestCancellation = React.lazy(() => import('./RequestCancellation'))
+const ModalsPage = React.lazy(() => import('./ModalsPage'))
+const Login = React.lazy(() => import('./Login'))
 
 const Router = ({ currentRole }) => {
   const [currentTheme, setCurrentTheme] = useState(themes.dark)
@@ -58,30 +59,32 @@ const Router = ({ currentRole }) => {
         </div>
 
         <ThemeContext.Provider value={{theme: currentTheme, toggleTheme: toggleTheme}}>
-          <Switch>
-            <Route path="/formik/:id" component={FormikForm}/>
-            <Route path="/redux_form/:id" component={ReduxForm}/>
-            <Route path="/list" component={List}/>
-            <Route path="/settings" component={Settings}/>
-            <Route path="/error" render={(routeProps) => (
-              <Boundary>
-                <ErrorBoundary/>
-              </Boundary>
-            )}/>
-            <Route
-              path="/admin"
-              render={renderProps => (
-                (currentRole === 'admin')
-                  ? <Admin {...renderProps} />
-                  : <Redirect to='/' />
-              )}
-            />
-            <Route path="/cancellation" component={RequestCancellation} />
-            <Route path="/specialization" component={ModalsPage} />
-            <Route path="/oauth/login" component={Login} />
-            <Route path="/oauth/callback" component={LoginCallback} />
-            <Route path="/" component={Home} />
-          </Switch>
+          <Suspense fallback={<div>Component is loading</div>}>
+            <Switch>
+              <Route path="/formik/:id" component={FormikForm}/>
+              <Route path="/redux_form/:id" component={ReduxForm}/>
+              <Route path="/list" component={List}/>
+              <Route path="/settings" component={Settings}/>
+              <Route path="/error" render={(routeProps) => (
+                <Boundary>
+                  <ErrorBoundary/>
+                </Boundary>
+              )}/>
+              <Route
+                path="/admin"
+                render={renderProps => (
+                  (currentRole === 'admin')
+                    ? <Admin {...renderProps} />
+                    : <Redirect to='/' />
+                )}
+              />
+              <Route path="/cancellation" component={RequestCancellation} />
+              <Route path="/specialization" component={ModalsPage} />
+              <Route path="/oauth/login" component={Login} />
+              <Route path="/oauth/callback" component={LoginCallback} />
+              <Route path="/" component={Home} />
+            </Switch>
+          </Suspense>
         </ThemeContext.Provider>
       </BrowserRouter>
     </div>
